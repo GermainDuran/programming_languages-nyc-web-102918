2891b7a27a5a7e27538485ac4202c575f13667cf
GERMAIN ARTURO DURAN TORRES <arturodurantorres@gmail.com>
2018-10-18 15:55:38 +0000

    automatically backed up by learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,15 +1,4 @@
-def reformat_languages(languages)
-  new_languages = {}
-  languages.each do |style, language|
-    language.each do |name, attributes|
-    new_languages[name] ||= attributes
-    new_languages[name][:style]=[]
 
-    
-    end
-  end
-  new_languages
-end
 
 def reformat_languages(languages)
   new_languages = {}
