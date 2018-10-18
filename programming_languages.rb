6b0c8a4cc18b387671b222ba866fdf5c540556c4
GERMAIN ARTURO DURAN TORRES <arturodurantorres@gmail.com>
2018-10-18 15:39:38 +0000

    automatically backed up by learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -3,38 +3,9 @@ def reformat_languages(languages)
   new_hash={}
   
   
-  new_hash[:ruby]=
+  new_hash[:ruby]={type: "interpreted", style: [:oo]}
   
   
-  {
-  :ruby => {
-    :type => "interpreted",
-    :style => [:oo]
-  },
-  :javascript => {
-    :type => "interpreted",
-    :style => [:oo, :functional]
-  },
-  :python => {
-    :type => "interpreted",
-    :style => [:oo]
-  },
-  :java => {
-    :type => "compiled",
-    :style => [:oo]
-  },
-  :clojure => {
-    :type => "compiled",
-    :style => [:functional]
-  },
-  :erlang => {
-    :type => "compiled",
-    :style => [:functional]
-  },
-  :scala => {
-    :type => "compiled",
-    :style => [:functional]
-  }
-}
+  
   
 end
